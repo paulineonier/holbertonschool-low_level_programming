4185cfb544f3ad4b5c8bf9a08d32429b37afa53d
paulineonier <pauline.onier@outlook.fr>
2024-02-19 03:41:27 -0800

    task 4² correction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  *  * main - Entry point
  *   *
- *    * Return: always 0
+ *    * Return:always 0
  *     */ int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
