356190d9938c1178fb47e72325f5d2678b7296a9
paulineonier <pauline.onier@outlook.fr>
2024-02-19 03:43:59 -0800

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return:always 0
- *     */ int main(void)
+ * main - Entry point
+ *
+ * Return:always 0
+ */ int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
