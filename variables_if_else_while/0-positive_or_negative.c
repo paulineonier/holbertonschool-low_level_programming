680e81afa7bc36aae6a8d8793bfd26742c3902b7
paulineonier <pauline.onier@outlook.fr>
2024-02-20 02:34:02 -0800

    task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,19 +11,20 @@ int main(void)
 
        	/* Check if the variable n is positive, zero, or negative */
 	
-	printf("%d is positive", n);
 
        	if (n > 0)
-       	{ printf("The number %d is positive.\n", n);
+
+	{
+	       	printf("%d is positive\n", n);
        	}
        	else if (n == 0)
        	{
-	printf("The number %d is zero.\n", n);
+		printf("%d is zero\n", n);
        	}
        	else
        	{
-	printf("The number %d is negative.\n", n);
-							        }
+		printf("%d is negative\n", n);
+       	}
 
        	return 0;
 }
