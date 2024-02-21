1e67f72c96b3d28e709ae6f32b76839c04e34ad9
paulineonier <pauline.onier@outlook.fr>
2024-02-21 02:22:57 -0800

    task 0 last correction

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-/** 
- * Return: Always 0 (success) 
-*/
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -11,11 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	/* Get the last digit of n */
-	int last_digit = n % 10
 
-	/* Check if the variable n is positive, zero, or negative */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -23,10 +21,11 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}
-	else
+											}
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
