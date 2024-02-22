4ec19f1886347ceefa37ea7f0c8a09b4a7c65c64
paulineonier <pauline.onier@outlook.fr>
2024-02-22 12:10:14 +0000

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
