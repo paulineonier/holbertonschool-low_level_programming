d5f76bb5a71fbb83997d7a062b9211bd484148e5
paulineonier <pauline.onier@outlook.fr>
2024-02-23 13:46:36 +0000

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * Main - Entry point
+ * print_last_digit - print the last digit of a number
+ * @n: int type number
+ * Return: return value of last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+}
