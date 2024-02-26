4a38b26e4963d91268c1c30e9304d85110fecc39
paulineonier <pauline.onier@outlook.fr>
2024-02-26 11:35:15 +0000

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integers
+ * @b: second integers
+ *
+ * Return: result of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
