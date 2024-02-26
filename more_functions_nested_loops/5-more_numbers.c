49b65a78e467684ed3b8e1399df28c6c7b4e30d7
paulineonier <pauline.onier@outlook.fr>
2024-02-26 14:54:30 +0000

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ */
+
+void more_numbers(void)
+{
+	char i, j;
+
+	for (i = '0'; i <= 10; i++)
+	{
+		for (j = '0'; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+
+			{
+				_putchar((j % 10) + '0');
+			}
+
+		_putchar('\n');
+		}
+	}
+}
+
