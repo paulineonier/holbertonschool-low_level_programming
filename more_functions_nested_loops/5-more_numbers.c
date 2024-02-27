6a1ca7680d017af4f7b06f43082a027293269f22
paulineonier <pauline.onier@outlook.fr>
2024-02-27 13:14:27 +0000

    correction task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@ void more_numbers(void)
 {
 	char i, j;
 
-	for (i = '1'; i <= 10; i++)
+	for (i = '1'; i <= 9; i++)
 	{
-		for (j = '0'; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
+			if (j >= 9)
 			{
 				_putchar((j / 10) + '0');
 			}
