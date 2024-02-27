ca3ac177f567394396039eddcb993bb0f5252e67
paulineonier <pauline.onier@outlook.fr>
2024-02-27 14:22:14 +0000

    task 9 !!!!!

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" Fizz ");
+			printf("FizzBuzz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 3 == 0)
 		{
-			printf(" Buzz ");
+			printf("Fizz ");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			printf(" FizzBuzz ");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
+			printf("Buzz ");
 		}
 		else
 		{
