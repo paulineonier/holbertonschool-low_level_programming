7a05387d35e3a3668286fb5cdad34562dc5255e8
paulineonier <pauline.onier@outlook.fr>
2024-02-28 14:47:40 +0000

    task 6

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -14,19 +14,25 @@ void rev_string(char *s)
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0') /* calcul de la len de la chaine */
 	{
 		i++;
 	}
 
-	j = i - 1;
-	h = j / 2;
+	j = i - 1; /* indice du dernier caractere de la chaine */
+	h = j / 2; /* indice correspondant moitie de la chaine */
 	while (j >= 0 && h >= 0)
+	/* boucle pour inversion de la chaine en utilisant 2 pointeurs */
 	{
 		first = s[j - h];
+	/*dernier caractere - moiti√ ©chaine devient first */
 		last = s[h];
+	/* dernier devient moiti√©*/
 		s[h] = first;
+	/* echange s'effectue, remplace caractere symetrique du cote oppose */
 		s[j - h] = last;
+	/* first devient last, finalise l'echange */
 		h--;
+	/* decremente pour passer a la paire suivante */
 	}
 }
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * puts2 - prints every other character starting with first character
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0; /* initialise compteur a 0 pour commencer au first caractere */
+	while (str[i] != '\0') /* boucle parcours la chaine de caractere */
+	{
+		if (i % 2 == 0) /* verifie si indice est paire */
+		{
+			_putchar(str[i]);/* print les indice paire */
+			i++;
+		}
+		_putchar('\n');
+	}
+}
