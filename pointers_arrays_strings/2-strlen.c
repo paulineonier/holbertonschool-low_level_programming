aefc1aae84c61de5aa25f4ff73b1358a6d59c1d6
paulineonier <pauline.onier@outlook.fr>
2024-02-28 10:23:25 +0000

    task 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strlen - returns the length of a string
@@ -8,12 +9,13 @@
 
 int _strlen(char *s)
 {
-	int i;
+	int len = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-
+	for (; *s != '\0'; s++)
 	{
-	return (i);
+		len++;
 	}
-	return (0);
+
+	return (len);
 }
+
