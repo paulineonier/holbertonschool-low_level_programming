40fc500c6914ad694fef5fbfe4ee4c8027e2110c
paulineonier <pauline.onier@outlook.fr>
2024-02-28 14:03:43 +0000

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,36 +10,23 @@
 void rev_string(char *s)
 {
 	int i, j, h;
-	char *a, tmp;
+	char first, last;
 
-	a = s; /* sauvegarde pointeur du string dans a */
+	i = 0;
 
-		while (s[j] != '\0')
-	/* compte nbre de caractere dans la chaine */
-		{
-			j++;
-		}
-		for (h = 1; h < j; h++)
-		{
-			a++;
-/* deplace pointeur a vers fin de chaine en l'avancant de h positions */
-		}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-		for (i = 0; i < (j / 2); i++)
-/*
- * inverse les caracteres,
- * continue tant que valeur de i < a la moitie de la len totale de la chaine
- * qd inverse chaine caractere traiter par paire avec son symetrique
- */
-		{
-/* partie du code qui echange les caracteres dans la chaine pour inversion */
-			tmp = s[i];
-/* caractere de la chaine sauvegarder dans variabale temporaire */
-			s[i] = *a;
-	/* echange la chaine avec position actuelle de a */
-			*a = tmp;
-/* remplace caractere sauvegarde dans a par caractere sauvegardÃ dans tmp */
-			a--;
-	/* deplace le pointeur a vers debut de la chaine */
-		}
+	j = i - 1;
+	h = j / 2;
+	while (j >= 0 && h >= 0)
+	{
+		first = s[j - h];
+		last = s[h];
+		s[h] = first;
+		s[j - h] = last;
+		h--;
+	}
 }
