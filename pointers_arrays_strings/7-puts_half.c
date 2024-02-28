01971d6facf8cb0851a157d916d5c1cfe5c07199
paulineonier <pauline.onier@outlook.fr>
2024-02-28 16:02:58 +0000

    task 7 correction

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	}
 	h = i; /* last caractere */
 	j = h / 2; /* moitié de la chaine */
-	while (j <= h)
+	while (j < h)
 	/* tant que moitie est inf ou egale au dernier caractere */
 	{
 		_putchar(str[j]); /* affiche indice */
