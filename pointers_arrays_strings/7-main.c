98fc079344026e82db0b88cca28081afc9123dfc
paulineonier <pauline.onier@outlook.fr>
2024-03-01 14:15:19 +0000

    task 7

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * leet - function that encodes a string into 1337
+ * @src: string
  *
- * Return: Always 0.
+ * Return: src
  */
-int main(void)
-{
-	char *str;
 
-	str = "0123456789";
-	puts_half(str);
-	return (0);
+char *leet(char *src)
+{
+
+	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	int i, j;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			if (src[j] == a[i])
+				src[j] = b[i];
+		}
+	}
+
+	return (src);
 }
