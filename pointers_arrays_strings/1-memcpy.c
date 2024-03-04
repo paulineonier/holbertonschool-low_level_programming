3f2c3e1ec5cd6b3429aeaeab90e3e29fe7d874ee
paulineonier <pauline.onier@outlook.fr>
2024-03-04 14:52:15 +0100

    task 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	if (n > 0)
+	int bytes = n;
+
+	if (bytes > 0)
 	{
-		for (i = 0; i < n; i++)
+		int i;
+
+		for (i = 0; i < bytes; i++)
 			src[i] = dest[i];
 	}
-	return (dest)
+	return (dest);
 }
