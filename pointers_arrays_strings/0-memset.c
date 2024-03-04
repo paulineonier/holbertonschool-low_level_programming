a81b2c6126596d22807a8cf3fafce6d782fdf917
paulineonier <pauline.onier@outlook.fr>
2024-03-04 13:31:29 +0100

    task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with the constant byte
+ * @b: constant value
+ * @s: pointer to  memory area
+ * @n: number of bytes to fill
+ *
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	if (n > 0)
+	{
+		int i;
+		for (i = 0; i < n; i++)
+			s[i] = b;
+	}
+	return (s)
+}
