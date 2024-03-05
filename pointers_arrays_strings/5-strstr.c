4ebb0fd65d18e7894ba314e2c9a69afb925027d5
paulineonier <pauline.onier@outlook.fr>
2024-03-05 09:44:31 +0000

    task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
-#include "main;h"
+#include "main.h"
+#include <stddef.h>
 
 /**
  * _strstr - locates a substring
@@ -11,10 +12,43 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
-	int j, h;
+	int j;
 
-	while (haystack[i] < '\0' && haystack[i] != needle[j])
-		i++;
-	if (haystack[i] == needle[j])
-		return (needle[j];
-	else 
+	/* Parcours de la chaîne haystack */
+
+	while (haystack[i] != '\0')
+	{
+	/*  Si le caractère actuel de haystac pas égal a1er caractère deneedle */
+		if (haystack[i] != needle[0])
+		{
+			i++;
+		}
+		else
+		{
+	/**
+	* Si 1er caract' n trouvé dans  verifie correspondance sous-chaine
+	* i + j = position actuelle qu'on verifie dans haystack
+	*/
+			j = 0;
+			while (needle[j] != '\0' && haystack[i + j] == needle[j])
+			{
+				j++;
+			}
+
+	/* Si sous-chaîne=  entièrement trouvée, retourun pointeur dt sous-chaîn*/
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
+			else
+			{
+				/* Sinon, continue parcourir la chaîne haystack */
+				i++;
+			}
+		}
+	}
+
+	/* Si aucune occurrence de la sous-chaîne n'a été trou */
+	return (NULL);
+}
+
