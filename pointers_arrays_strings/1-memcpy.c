67839607586ed54f35491093707e129d28155bb5
paulineonier <pauline.onier@outlook.fr>
2024-03-06 09:51:34 +0000

    task 1 correction

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,15 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int size = n; /* only accept positive sizes */
 
-	for (i = 0; i < n; i++)
+	if (size > 0)
 	{
-		*(src + 1) = *(dest + 1);
+		int i;
+
+		for (i = 0; i < size; i++)
+			dest[i] = src[i];
 	}
+
 	return (dest);
 }
