31b79ee70c3be301c7523e21272f57d4e6fd1cc6
paulineonier <pauline.onier@outlook.fr>
2024-03-06 09:30:47 +0000

    task 5 correction

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,44 +11,25 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int j;
+	int i, j, c;
 
-	/* Parcours de la chaîne haystack */
-
+	i = 0;
+	c = 0;
 	while (haystack[i] != '\0')
 	{
-	/*  Si le caractère actuel de haystac pas égal a1er caractère deneedle */
-		if (haystack[i] != needle[0])
+		j = 0;
+		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
+				&& needle[j + c] == haystack[i + c])
 		{
-			i++;
+			if (haystack[i + c] != needle[j + c])
+				break;
+			c++;
 		}
-		else
-		{
-	/**
-	* Si 1er caract' n trouvé dans  verifie correspondance sous-chaine
-	* i + j = position actuelle qu'on verifie dans haystack
-	*/
-			j = 0;
-			while (needle[j] != '\0' && haystack[i + j] == needle[j])
-			{
-				j++;
-			}
-
-	/* Si sous-chaîne=  entièrement trouvée, retourun pointeur dt sous-chaîn*/
-			if (needle[j] == '\0')
-			{
-				return (&haystack[i]);
-			}
-			else
-			{
-				/* Sinon, continue parcourir la chaîne haystack */
-				i++;
-			}
-		}
+		if (needle[j + c] == '\0')
+			return (&haystack[i]);
+		j++;
+		i++;
 	}
 
-	/* Si aucune occurrence de la sous-chaîne n'a été trou */
 	return (NULL);
 }
-
