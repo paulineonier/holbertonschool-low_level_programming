7b6d60872a1c53352d95ca0a8c9f4dece7fd8d8c
paulineonier <pauline.onier@outlook.fr>
2024-03-07 14:11:14 +0000

    task 0 correction

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: array who tell me how much arguments
+ * @argv: arguments
+ * Return: 0 on success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
