1dc9bb181a2fb88b0f8cd604cc4ea5a88bb50db9
paulineonier <pauline.onier@outlook.fr>
2024-03-07 13:58:37 +0000

    task 0

---
+++ argc_argv/.0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguments
+ * @argv: array
+ * Return: 0
+ */
+
+int main (int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
