3d047e6378305c889d8442cc5161f53e4808aed8
paulineonier <pauline.onier@outlook.fr>
2024-03-14 11:25:13 +0000

    task 3 add comments

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,22 +15,26 @@ int *array_range(int min, int max)
 	int i;
 	int n = (max - min + 1);
 
-	if (min > max) /* validate input */
+	if (min > max) /* verifie si min > max si oui return NULL */
 	{
 		return (NULL);
 	}
-	/* malloc and check for error */
+
 	ptr = malloc(sizeof(int) * n);
+	/**
+	 * alloue dynamiquement de la memoire pour stocker n
+	 * n = diff entre max et min avce +1 pour inclure max
+	 */
 
-	if (ptr == NULL)
+	if (ptr == NULL) /* si allocation echoue renvoie NULL */
 	{
 		return (NULL);
 	}
 
-	/* set values */
 	for (i = 0; i < n; i++)
+	/* initialise les valeurs du tableau avec entier de min a max */
 	{
 		ptr[i] = min++;
 	}
-	return (ptr);
+	return (ptr); /* renvoie pointeur vers la tableau */
 }
