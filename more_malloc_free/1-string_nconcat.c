911ae1fc76d964d8f356434c964f45ba1b9ba471
paulineonier <pauline.onier@outlook.fr>
2024-03-14 09:58:00 +0000

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,41 +11,42 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	int bytes, len, i, j;
-
-	bytes = n;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (bytes < 0) /* if negative n bytes */
-	{	
+
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-	}
-	if (bytes >= _strlen(s2)) /* if bigger than s2 */
-	{	
-		bytes = _strlen(s2);
 
-	len = _strlen(s1) + bytes + 1; /* +1 for null pointer */
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
 	}
-
-	if (ptr == NULL)
-	{	
-		return (NULL);
-	}
-
-	for (i = 0; s1[i] != '\0'; i++) /* concat */
-	{	
-		ptr[i] = s1[i];
-	}
-	for (j = 0; j < num; j++)
-	{	
-		ptr[i + j] = s2[j];
-		ptr[i + j] = '\0';
-	}
-	return (ptr);
+	str[j] = '\0';
+	return (str);
 }
