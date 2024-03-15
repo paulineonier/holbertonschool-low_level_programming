716dd50ad5de01f31b5b70ae72dca998cf3e8f70
paulineonier <pauline.onier@outlook.fr>
2024-03-15 09:17:31 +0000

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: struct dog.
+ * if fails, returns NULL.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p_dog;
+	int i, cname, cowner;
+
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
+	{
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (cname = 0; name[cname]; cname++)
+		;
+
+	for (cowner = 0; owner[cowner]; cowner++)
+		;
+
+	p_dog->name = malloc(cname + 1);
+	p_dog->owner = malloc(cowner + 1);
+
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < cname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < cowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
+}
