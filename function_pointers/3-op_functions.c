9ec3168b30c981a4622823c5f1f51c5ed2dce72f
paulineonier <pauline.onier@outlook.fr>
2024-03-19 09:42:38 +0000

    task 3, file 2

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - addition
+ * @a: entier
+ * @b: entier
+ * Return: résultat de l'addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - soustraction
+ * @a: entier
+ * @b: entier
+ * Return: résultat de la soustraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: entier
+ * @b: entier
+ * Return: résultat de la multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: entier
+ * @b: entier
+ * Return: résultat de la division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo pour le reste
+ * @a: entier
+ * @b: entier
+ * Return: résultat du modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
